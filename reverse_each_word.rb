1da1afe5cdc3b2ddce4e72b1034240ed85d61e6a
Laura Felce <crepu83@yahoo.it>
2018-11-19 13:31:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word ("Hello there, and how are you?")
-"Hello there, and how are you?".each do |word|
+def reverse_each_word (phrase)
+phrase.each do |word|
   word.reverse
 end
-return "Hello there, and how are you?"
+return phrase
 end
