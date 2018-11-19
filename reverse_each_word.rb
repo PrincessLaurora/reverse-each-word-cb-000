e4d366347e528fc79f40777187c87bc3a93fe670
Laura Felce <crepu83@yahoo.it>
2018-11-19 15:14:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,13 @@ end
 new_array.join(" ")
 end
 
+#Collect method wth {}
 
 #def reverse_each_word (phrase)
 #phrase.split.collect {|word| word.reverse}.join(" ")
 #end
+
+#Each method
 
 #def reverse_each_word (phrase)
 #new_array = []
