327fe6aacf928003ba6c9a554849b0cb770bc7c3
Laura Felce <crepu83@yahoo.it>
2018-11-19 15:03:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word (phrase)
 new_array = phrase.split
-new_array.collect do |word|
+result = new_array.collect do |word|
 word.reverse
 end
-new_array.join(" ")
+result.join(" ")
 end
 
 
