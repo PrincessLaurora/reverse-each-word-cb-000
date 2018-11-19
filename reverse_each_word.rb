f29712c3df27f2b8503cc4f80d2cf853719b1d8e
Laura Felce <crepu83@yahoo.it>
2018-11-19 14:27:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word (phrase)
 new_phrase = ""
 phrase.split(", ").each do |word|
-new_phrase.insert(0, word.reverse)
+new_phrase.push("word.reverse")
+
 end
 return new_phrase
 end
